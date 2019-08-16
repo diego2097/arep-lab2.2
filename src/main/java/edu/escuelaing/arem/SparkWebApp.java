190cc1579f35ee40bef48601c1f53887df24ed1b
unknown <2137516@labinfo.is.escuelaing.edu.co>
2019-08-16 12:39:25 -0500

    Proyecto completo

---
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arem;
+import static spark.Spark.*;
+
+
+/**
+ *
+ * @author 2137516
+ */
+public class SparkWebApp {
+
+    public static void main(String[] args) {
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku");
+    }
+ 
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+        return Integer.parseInt(System.getenv("PORT"));
+    }
+    return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
