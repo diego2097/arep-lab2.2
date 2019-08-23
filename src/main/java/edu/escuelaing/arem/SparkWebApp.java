131cde5adf130b2728f105309511e7259d28251b
diego2097 <34497493+diego2097@users.noreply.github.com>
2019-08-22 21:51:29 -0500

    Update SparkWebApp.java

--- src/main/java/edu/escuelaing/arem/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -66,29 +66,5 @@ public class SparkWebApp {
                 + "</html>";
        
         return pagina;
-    }
-
-   public static LinkedList<Double> calculo(String numeros)throws FileNotFoundException, IOException{  
-        String[] list = numeros.split(",");
-        
-        float suma=0;
-        LinkedList<Double> lista = new LinkedList<>();
-        for (int i=0; i<list.length;i++){ 
-            Double number = Double.parseDouble(list[i]);
-            lista.add(number);
-            suma+=number;
-        }	
-        double media=suma/list.length;
-        double numerador=0;
-        
-        for (int i=0; i<lista.size();i++){
-            numerador+=Math.pow(lista.get(i)-media,2);
-        }
-        
-        double desv=Math.sqrt(numerador/(lista.size()-1));
-        LinkedList<Double> rta  = new LinkedList<>();
-        rta.add(media);
-        rta.add(desv);
-        return rta; 
-    }    
+    } 
 }
